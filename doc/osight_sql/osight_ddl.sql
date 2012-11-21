a4c84d9a09db0c0321d9a4c812cdf4eb5cda9fbc
rodneytt at home <rodneytt@sina.com>
2012-11-21 22:38:45 +0800

    建表语句
    彩票记录表

---
+++ doc/osight_sql/osight_ddl.sql
@@ -0,0 +1,14 @@
+﻿CREATE  table lottery(
+       id int primary key,
+       date date not null,
+       type_id int DEFAULT 0,
+       number varchar(100)
+)
+create table lottery_number(
+       id int not null AUTO_INCREMENT,
+       lottery_id int not null,
+       number int not null,
+       name varchar(200),
+       PRIMARY key(id),
+       FOREIGN KEY (lottery_id) REFERENCES lottery (id)
+)
