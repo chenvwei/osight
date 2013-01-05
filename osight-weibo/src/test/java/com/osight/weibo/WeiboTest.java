d10ece99046c049700edc142ef23d93e2a9bfac2
rodneytt at work <rodneytt@sina.com>
2013-01-05 13:54:32 +0800

    增加微博模块
    Signed-off-by: rodneytt at work <rodneytt@sina.com>

---
+++ osight-weibo/src/test/java/com/osight/weibo/WeiboTest.java
@@ -0,0 +1,24 @@
+/*
+ * Created on 2013-1-5
+ */
+package com.osight.weibo;
+
+
+import org.junit.Test;
+
+/**
+ * @author chenw
+ * @version $Id$
+ */
+public class WeiboTest {
+
+    @Test
+    public void test() {
+
+    }
+
+    @Test
+    public void testConn(){
+        
+    }
+}
